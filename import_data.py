d12067832b3af4af6cf87ae45b4c92939201e533
Kad <kletts2000@gmail.com>
2017-04-25 11:06:49 -0400

    Update import_data.py

--- import_data.py
+++ import_data.py
@@ -170,7 +170,7 @@ def menu():
 	while (choice != 5):
 		print "##################################################################################"
 		print "#                        What would you like to do?                              #"
-		print "#################################################################################"
+		print "##################################################################################"
 		print "#1. 	Insert all Data from site                                               #"
 		print "#2. 	Insert a single page from site                                          #"
 		print "#3. 	Answer question with data in database(ensure all data is inserted first)#"
